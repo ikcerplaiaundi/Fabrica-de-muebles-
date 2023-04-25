a2dc7d72d32a577fff3f9d0c6ec983c669cc027a
Polskivus <polako_irun@icloud.com>
2023-04-25 11:57:24 +0200

    Creado la clase proveedores (Suppliers 0.0.1)

--- src/main/java/modelo/DAO/Proveedor.java
+++ src/main/java/modelo/DAO/Proveedor.java
@@ -1,5 +1,51 @@
 package modelo.DAO;
 
 public class Proveedor {
+	// This is the class of the supplier of the goods("Piezas")
+	private String direccion;
+	private String cif;
+	private String telefono;
+	private String nombreProveedor;
+	private int idProveedor;
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getCif() {
+		return cif;
+	}
+
+	public void setCif(String cif) {
+		this.cif = cif;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getNombre() {
+		return nombreProveedor;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombreProveedor = nombre;
+	}
+
+	public int getIdProveedor() {
+		return idProveedor;
+	}
+
+	public void setIdProveedor(int idProveedor) {
+		this.idProveedor = idProveedor;
+	}
 
 }
