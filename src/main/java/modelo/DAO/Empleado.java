22afdfb12c1c4e9e1ed6993597af3517f7513c50
plaiaundi <plaiaundi@HZ309808.H012108.NET>
2023-04-25 10:37:30 +0200

    empleado 0.0.2

---
+++ src/main/java/modelo/DAO/Empleado.java
@@ -0,0 +1,49 @@
+package modelo.DAO;
+
+public class Empleado {
+	//all emplollers need to be an post  and role
+	private int idEmpleado;
+	private String nombreEmpleado;
+	private String puesto;
+	private int mgr;
+	private Rol rol;
+	private String contraseña;
+	//get/set
+	public int getIdEmpleado() {
+		return idEmpleado;
+	}
+	public void setIdEmpleado(int idEmpleado) {
+		this.idEmpleado = idEmpleado;
+	}
+	public String getNombreEmpleado() {
+		return nombreEmpleado;
+	}
+	public void setNombreEmpleado(String nombreEmpleado) {
+		this.nombreEmpleado = nombreEmpleado;
+	}
+	public String getPuesto() {
+		return puesto;
+	}
+	public void setPuesto(String puesto) {
+		this.puesto = puesto;
+	}
+	public int getMgr() {
+		return mgr;
+	}
+	public void setMgr(int mgr) {
+		this.mgr = mgr;
+	}
+	public Rol getRol() {
+		return rol;
+	}
+	public void setRol(Rol rol) {
+		this.rol = rol;
+	}
+	public String getContraseña() {
+		return contraseña;
+	}
+	public void setContraseña(String contraseña) {
+		this.contraseña = contraseña;
+	}
+	
+}
