35be9c6d9756d157e5e3048bbb9999346b34276a
plaiaundi <plaiaundi@HZ309808.H012108.NET>
2023-05-09 14:28:59 +0200

    chose products(0.1.4)

--- src/main/java/servlets/ChooseProducts.java
+++ src/main/java/servlets/ChooseProducts.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 import modelo.DAO.Producto;
 import modelo.DTO.GestorBDD;
 
@@ -19,39 +18,55 @@ import modelo.DTO.GestorBDD;
 @WebServlet("/ChooseProducts")
 public class ChooseProducts extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ChooseProducts() {
-        super();
-        
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		
-		GestorBDD gdbb =new GestorBDD();
+	public ChooseProducts() {
+		super();
+
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		GestorBDD gdbb = new GestorBDD();
 		gdbb.abrirConexion();
-		ArrayList <Producto> productos = new ArrayList <Producto>();
-		productos=	gdbb.pullProductos(" /**/ ");
-		 
-		 
+		ArrayList<Producto> productos = gdbb.pullProductos(" /**/ ");
 		gdbb.cerrarConexion();
-		
+
 		request.setAttribute("productos", productos);
 		request.getRequestDispatcher("ChooseProducts.jsp").forward(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		//Search the stock of each product type for serl
+		GestorBDD gdbb = new GestorBDD();
+		gdbb.abrirConexion();
+		ArrayList<Producto> productos = gdbb.pullProductos(" /**/ ");
+		gdbb.cerrarConexion();
+		ArrayList<Producto> productosCompra = new ArrayList<Producto>();
+		Producto producto;
+		
+		for (int i = 0; productos.size() > i; i++) {
+			if (null != request.getParameter("" + productos.get(i).getIdProducto())) {
+				producto = new Producto();
+				producto.setIdProducto(productos.get(i).getIdProducto());
+				producto.setStockProducto(
+						Integer.parseInt(request.getParameter("" + productos.get(i).getIdProducto())));
+				productosCompra.add(producto);
+				
+			}
+		}
 
+	}
 }
